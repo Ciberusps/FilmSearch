8681c934372c2cb17227ba063ba04ecc85994a7e
Ciberusps <Ciberus.ps@gmail.com>
2016-05-29 22:29:19 +0300

    -Все не робит

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
+using System.Net.Cache;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using EasyHttp.Http;
-using MaterialMenu;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 namespace Test
 {
@@ -69,6 +67,7 @@ namespace Test
         private void CloseClick(object sender, RoutedEventArgs e)
         {
             Menu.Hide();
+            
         }
 
         /*private void DefaultClick(object sender, RoutedEventArgs e)
@@ -120,7 +119,7 @@ namespace Test
         private void MoviesGenresList_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             MovieGenre movieGenre = (MovieGenre)MoviesGenresList.SelectedItem;
-            movieGenre.GetPopularFilmsInGenre();
+            movieGenre.GetMoviesGenre();
 
             ContentTabControl.SelectedItem = GenreTab;
 
@@ -135,15 +134,25 @@ namespace Test
         private void GenreMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             Movie movie = (Movie)GenreList.SelectedItem;
-            MessageBox.Show(movie.nameRU + /*" " + movie.nameEN + " " + movie.country +*/ " " + movie.id);
+            movie.GetMovie();
+
+            ContentTabControl.SelectedItem = FilmDescriptionTab;
+
+//            MovieDescription.DataContext = movie;
+
+            /*MyBitmapImage.Source = new BitmapImage(new Uri(movie.bigImage));
+            NameRU.Text = movie.nameRU;
+            Genre.Text = movie.genre;
+            Rating.Text = movie.rating;*/
+            //            MessageBox.Show(movie.NameRU + /*" " + movie.nameEN + " " + movie.country +*/ " " + movie.id + "" + movie.bigImage);
         }
     }
 
     public class MovieGenre
     {
-        public static string popularFilmsRequest =
+        public static string POPULAR_MOVIE_REQUEST =
             "http://api.kinopoisk.cf/getTop?type=kp_item_top_popular_films&genreID=";
-        public static string topBestFilmsRequest =
+        public static string TOP_BEST_MOVIE_REQUEST =
             "http://api.kinopoisk.cf/getTop?type=kp_item_top_best_films&genreID=";
 
         public string image { get; set; }
@@ -155,36 +164,15 @@ namespace Test
 
         public ObservableCollection<Movie> genreMovies { get; set; }
 
-        public void GetPopularFilmsInGenre()
+        public void GetMoviesGenre()
         {
-            /*HttpWebRequest popularFilmsHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(popularFilmsRequest + id);
-//            popularFilmsHttpWebRequest.ContentType = @"text/html; charset=windows-1251";
-            HttpWebResponse popularFilmsHttpWebResponse = (HttpWebResponse)popularFilmsHttpWebRequest.GetResponse();
+            string responseFromServer = Caching.CacheRequest(AppData.movieRequest == AppData.MovieRequest.TopBestMovies
+                ? TOP_BEST_MOVIE_REQUEST + id
+                : POPULAR_MOVIE_REQUEST + id);
 
-            // Get the stream containing content returned by the server.
-            Stream dataStream = popularFilmsHttpWebResponse.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();*/
+                        MessageBox.Show(responseFromServer);
 
-            string responseFromServer;
-
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(popularFilmsRequest + id);
-            using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
-            {
-                using (Stream resStream = response.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(resStream, Encoding.GetEncoding(response.CharacterSet));
-                    responseFromServer = reader.ReadToEnd();
-                }
-            }
-
-            if (responseFromServer == null) throw new ArgumentNullException(nameof(responseFromServer));
-
-            MessageBox.Show(responseFromServer);
-
-            JObject jRoot = JObject.Parse(responseFromServer); 
+            JObject jRoot = JObject.Parse(responseFromServer);
             JArray jMovies = jRoot["items"] as JArray;
 
             genreMovies = new ObservableCollection<Movie>();
@@ -197,39 +185,218 @@ namespace Test
                 if (movie["year"] != null) currentMovie.year = movie["year"].ToString();
                 if (movie["filmLength"] != null) currentMovie.filmLength = movie["filmLength"].ToString();
                 if (movie["country"] != null) currentMovie.country = movie["country"].ToString();
-                if (movie["genre"] != null) currentMovie.genre = movie["genre"].ToString();
-                if (movie["rating"] != null) currentMovie.rating = movie["rating"].ToString();
-                if (movie["nameRU"] != null) currentMovie.nameRU = movie["nameRU"].ToString();
+                if (movie["Genre"] != null) currentMovie.genre = movie["Genre"].ToString();
+                if (movie["Rating"] != null) currentMovie.rating = movie["Rating"].ToString();
+                if (movie["NameRU"] != null) currentMovie.nameRU = movie["NameRU"].ToString();
                 if (movie["nameEN"] != null) currentMovie.nameEN = movie["nameEN"].ToString();
                 if (movie["posterURL"] != null) currentMovie.posterURL = movie["posterURL"].ToString();
                 if (movie["videoURL"] != null) currentMovie.videoURL = movie["videoURL"].ToString();
 
+                currentMovie.bigImage = Movie.FILM_BIG_IMAGE_REQUEST + currentMovie.id + ".jpg";
+                currentMovie.smallImage = Movie.FILM_IMAGE_REQUEST + currentMovie.id + ".jpg";
+
                 genreMovies.Add(currentMovie);
             }
-
-
-        }
-
-        public void GetTopBestFilmsInGenre()
-        {
-
         }
     }
 
-    public class Movie
+    public class Movie 
     {
-        public static string filmBigImageRequest = "http://st.kp.yandex.net/images/film_big/";
-        public static string filmImageRequest = "http://st.kp.yandex.net/images/film/";
+        public const string FILM_BIG_IMAGE_REQUEST = "http://st.kp.yandex.net/images/film_big/";
+        public const string FILM_IMAGE_REQUEST = "http://st.kp.yandex.net/images/film/";
+        public const string FILM_REQUEST = "http://api.kinopoisk.cf/getFilm?filmID=";
+        public const string FILM_GALLERY_PREVIEW = "http://st.kp.yandex.net/images/";
 
-        public int id;
-        public string year;
-        public string filmLength;
-        public string country;
-        public string genre;
-        public string rating;
-        public string nameRU;
-        public string nameEN;
-        public string posterURL;
-        public string videoURL;
+        //Minimal properties
+        public int id { get; set; }
+        public string year { get; set; }
+        public string filmLength { get; set; }
+        public string country { get; set; }
+        public string genre { get; set; }
+        public string rating { get; set; }
+        public string nameRU { get; set; }
+        public string nameEN { get; set; }
+        public string posterURL { get; set; }
+        public string videoURL { get; set; }
+
+        //All properties
+        public string webURL { get; set; }
+        public string slogan { get; set; }
+        public string description { get; set; }
+        public string ratingMPAA { get; set; }
+        public string ratingAgeLimits { get; set; }
+
+        //rentData
+        public RentData rentData;
+
+        public class RentData
+        {
+            public string premiereRU { get; set; }
+            public string distributors { get; set; }
+            public string premiereWorld { get; set; }
+            public string premiereWorldCountry { get; set; }
+            public string premiereDVD { get; set; }
+            public string premiereBluRay { get; set; }
+            public string distributorRelease { get; set; }
+        }
+
+        public BudgetData budgetData;
+
+        public class BudgetData
+        {
+            public string grossRU { get; set; }
+            public string grossUSA { get; set; }
+            public string grossWorld { get; set; }
+            public string budget { get; set; }
+        }
+
+        public ArrayList gallery;
+        public List<Creator> creators;
+
+        public class Creator
+        {
+            public string id { get; set; }
+            public string nameRU { get; set; }
+            public string nameEN { get; set; }
+            public string posterURL { get; set; }
+            public string professionText { get; set; }
+            public string professionKey { get; set; }
+        }
+
+        public News topNewsByFilm;
+
+        public class News
+        {
+            public string id { get; set; }
+            public string newsDate { get; set; }
+            public string newsImagePreviewURL { get; set; }
+            public string newsTitle { get; set; }
+            public string newsDescription { get; set; }
+        }
+
+        public ArrayList triviaData;
+
+        //Custom properties
+        public string bigImage { get; set; }
+        public string smallImage { get; set; }
+
+        public void GetMovie()
+        {
+            string responseFromServer = Caching.CacheRequest(FILM_REQUEST + id);
+
+//            MessageBox.Show(responseFromServer);
+
+            JObject jRoot = JObject.Parse(responseFromServer);
+            if (jRoot["webURL"] != null) webURL = jRoot["webURL"].ToString();
+            if (jRoot["slogan"] != null) slogan = jRoot["slogan"].ToString();
+            if (jRoot["description"] != null) description = jRoot["description"].ToString();
+            if (jRoot["ratingMPAA"] != null) ratingMPAA = jRoot["ratingMPAA"].ToString();
+            if (jRoot["ratingAgeLimits"] != null) ratingAgeLimits = jRoot["ratingAgeLimits"].ToString();
+
+            JObject jRentData;
+            if (jRoot["rentData"] != null)
+            {
+                jRentData = jRoot["rentData"] as JObject;
+                rentData = new RentData();
+                if (jRentData["premiereRU"] != null) rentData.premiereRU = jRentData["premiereRU"].ToString();
+                if (jRentData["distributors"] != null) rentData.distributors = jRentData["distributors"].ToString();
+                if (jRentData["premiereWorld"] != null) rentData.premiereWorld = jRentData["premiereWorld"].ToString();
+                if (jRentData["premiereWorldCountry"] != null) rentData.premiereWorldCountry = jRentData["premiereWorldCountry"].ToString();
+                if (jRentData["premiereDVD"] != null) rentData.premiereDVD = jRentData["premiereDVD"].ToString();
+                if (jRentData["premiereBluRay"] != null) rentData.premiereBluRay = jRentData["premiereBluRay"].ToString();
+                if (jRentData["distributorRelease"] != null) rentData.distributorRelease = jRentData["distributorRelease"].ToString();
+            }
+
+            JObject jBudgetData;
+            if (jRoot["budgetData"] != null)
+            {
+                jBudgetData = jRoot["budgetData"] as JObject;
+                budgetData = new BudgetData();
+                if (jBudgetData["grossRU"] != null) budgetData.grossRU = jBudgetData["grossRU"].ToString();
+                if (jBudgetData["grossUSA"] != null) budgetData.grossUSA = jBudgetData["grossUSA"].ToString();
+                if (jBudgetData["grossWorld"] != null) budgetData.grossWorld = jBudgetData["grossWorld"].ToString();
+                if (jBudgetData["budget"] != null) budgetData.budget = jBudgetData["budget"].ToString();
+            }
+
+            JObject jNews;
+            if (jRoot["topNewsByFilm"] != null)
+            {
+                jNews = jRoot["topNewsByFilm"] as JObject;
+                topNewsByFilm = new News();
+                if (jNews["id"] != null) topNewsByFilm.id = jNews["id"].ToString();
+                if (jNews["newsDate"] != null) topNewsByFilm.newsDate = jNews["newsDate"].ToString();
+                if (jNews["newsImagePreviewURL"] != null) topNewsByFilm.newsImagePreviewURL = jNews["newsImagePreviewURL"].ToString();
+                if (jNews["newsTitle"] != null) topNewsByFilm.newsTitle = jNews["newsTitle"].ToString();
+                if (jNews["newsDescription"] != null) topNewsByFilm.newsDescription = jNews["newsDescription"].ToString();
+            }
+
+            JArray jGallery;
+            if (jRoot["gallery"] != null)
+            {
+                jGallery = jRoot["gallery"] as JArray;
+                gallery = new ArrayList();
+                for (int i = 0; i < jGallery.Count; i++)
+                {
+                    if (jGallery[i]["preview"] != null)
+                        gallery.Add(FILM_GALLERY_PREVIEW + jGallery[i]["preview"]);
+                }
+            }
+
+            JArray jTriviaData;
+            if (jRoot["triviaData"] != null)
+            {
+                jTriviaData = jRoot["triviaData"] as JArray;
+                triviaData = new ArrayList();
+                for (int i = 0; i < jTriviaData.Count; i++)
+                {
+                    triviaData.Add(jTriviaData[i]);
+                }
+            }
+        }
     }
+
+    public class Caching
+    {
+        public static string CacheRequest(string resource)
+        {
+            string responseFromServer;
+
+            HttpRequestCachePolicy requestPolicy =
+                new HttpRequestCachePolicy(HttpCacheAgeControl.MaxAge,
+                TimeSpan.FromDays(3));
+
+            HttpWebRequest webRequest =
+                (HttpWebRequest)WebRequest.Create(resource);
+            webRequest.CachePolicy = requestPolicy;
+
+            using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
+            {
+                using (Stream resStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(resStream, Encoding.GetEncoding(response.CharacterSet));
+                    responseFromServer = reader.ReadToEnd();
+
+                    reader.Close();
+                    resStream.Close();
+                    response.Close();
+                }
+            }
+
+            if (responseFromServer == null) throw new ArgumentNullException(nameof(responseFromServer));
+
+            return responseFromServer;
+        }
+    }
+
+    public class AppData
+    {
+        public enum MovieRequest
+        {
+            TopBestMovies,
+            PopularMovies
+        }
+
+        public static MovieRequest movieRequest = MovieRequest.TopBestMovies;
+    }
+
 }
